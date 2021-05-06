ecd5d0fbd4979df56b87ff3163b35665a9288497
Gabriel Gene <gabrielgene97@gmail.com>
2021-05-06 09:59:04 -0300

    Admin url

--- apollo/client.js
+++ apollo/client.js
@@ -2,8 +2,9 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const HOST = 'http://localhost';
 const HOST2 = 'http://165.227.67.133';
+const HOST3 = 'https://admin.beandare.com.br'
 
-export const SERVER_URI = `${HOST2}:1337`;
+export const SERVER_URI = `${HOST3}:1337`;
 export const LOCAL_URI = `${HOST}:3000`;
 
 const client = new ApolloClient({
