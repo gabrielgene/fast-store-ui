de9f3b69ead0fd02663b1dcc25351612168094de
Gabriel Gene <gabrielgene97@gmail.com>
2021-05-06 10:08:40 -0300

    Update hosts

--- apollo/client.js
+++ apollo/client.js
@@ -3,13 +3,15 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 const HOST = 'http://localhost';
 const HOST2 = 'http://165.227.67.133:1337';
 const HOST3 = 'https://admin.beandare.com.br'
+const HOST4 = 'http://localhost:1337';
 
 export const SERVER_URI = `${HOST3}`;
 export const LOCAL_URI = `${HOST}:3000`;
+export const LOCAL_SERVER = `${HOST3}`;
 
 const client = new ApolloClient({
   ssrMode: true,
-  uri: `${LOCAL_URI}/graphql`,
+  uri: `${LOCAL_SERVER}/graphql`,
   cache: new InMemoryCache(),
 });
 
