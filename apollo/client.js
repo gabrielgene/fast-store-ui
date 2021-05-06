879d27f20d6ba10c67f66a87f3a7726b0de42c94
Gabriel Gene <gabrielgene97@gmail.com>
2021-05-06 10:00:43 -0300

    Update SERVER_URI

--- apollo/client.js
+++ apollo/client.js
@@ -1,10 +1,10 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const HOST = 'http://localhost';
-const HOST2 = 'http://165.227.67.133';
+const HOST2 = 'http://165.227.67.133:1337';
 const HOST3 = 'https://admin.beandare.com.br'
 
-export const SERVER_URI = `${HOST3}:1337`;
+export const SERVER_URI = `${HOST3}`;
 export const LOCAL_URI = `${HOST}:3000`;
 
 const client = new ApolloClient({
