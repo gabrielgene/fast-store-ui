f939619c02d8fdadd9da4cd9efa10c62b0e57753
Gabriel Gene <gabrielgene97@gmail.com>
2021-05-08 20:38:19 -0300

    Remove custom image loader

--- components/product-image/index.js
+++ components/product-image/index.js
@@ -1,5 +1,9 @@
 import Image from 'next/image';
 
+const loader = ({ src, width }) => {
+  return `https://ik.imagekit.io/9bgpx0om9/${src}?w=${width}`;
+};;
+
 export default function ProductImage(props) {
-  return <Image {...props} src={props.src} />;
+  return <Image {...props} src={props.src} loader={loader} />;
 }
--- next.config.js
+++ next.config.js
@@ -1,7 +1,1 @@
-module.exports = {
-  images: {
-    loader: 'imgix',
-    domains: ['ik.imagekit.io', 'localhost'],
-    path: 'https://ik.imagekit.io/9bgpx0om9/',
-  },
-};
+module.exports = {};
